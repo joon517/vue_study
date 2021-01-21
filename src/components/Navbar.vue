3130b581bb23b240d461bcfe2df55f8e42336ff4
Joonghyun <jhpark0517@hanmail.net>
2021-01-21 22:36:53 +0900

    study time 5:17

---
+++ src/components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+    <nav>
+        <v-navigation-drawer v-model="drawer" dark app mini-variant mini-variant-width="100" class="cyan darken-1">
+            <v-list>
+                <v-list-item class="mb-12">
+                    <v-list-item-action>
+
+                    </v-list-item-action>
+                </v-list-item>
+            </v-list>
+            <v-list flat>
+                <v-list-item router to="/">
+                    <v-list-item-content>
+                        <v-icon class="mb-2">dashboard</v-icon>
+                        <v-list-item-subtitle align="center">Dashboard</v-list-item-subtitle>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+    </nav>
+</template>
